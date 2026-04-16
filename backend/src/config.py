979a410733994ed39f19f823a10853ed5d7a245e
rinat <rinat.v06@gmail.com>
2026-04-16 15:37:23 -0500

    Added dockerfile stuff, not working yet

--- backend/src/config.py
+++ backend/src/config.py
@@ -9,3 +9,17 @@ class Config:
     """Application configuration object."""
 
     root_path: str = os.getenv("API_ROOT_PATH", default="")
+
+    # configuration values come from /docker-compose.yml
+    db_host: str = os.getenv("DB_HOST", "localhost")
+    db_port: int = int(os.getenv("DB_PORT", 5432))
+    db_name: str = os.getenv("DB_NAME", "finance")
+    db_user: str = os.getenv("DB_USER", "postgres")
+    db_password: str = os.getenv("DB_PASSWORD", "postgres")
+
+    @property
+    def database_url(self) -> str: # Constructs full database connection string
+        return (
+            f"postgresql://{self.db_user}:{self.db_password}"
+            f"@{self.db_host}:{self.db_port}/{self.db_name}"
+        )
