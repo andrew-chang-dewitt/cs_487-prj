db2f2f92396b327ebc5058fe6f1565ecc231be76
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2026-04-22 17:52:15 -0500

    feat(backend/docs): self-host `/docs` page resources

--- backend/src/app.py
+++ backend/src/app.py
@@ -4,6 +4,7 @@ from typing import Optional
 
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from fastapi_swagger import patch_fastapi
 
 from .config import Config
 from src.errors import TodoError
@@ -22,7 +23,12 @@ def create_app(config: Optional[Config] = None) -> FastAPI:
     app = FastAPI(
         # openapi_url="/api/openapi.json",
         root_path=config.root_path,
+        # disable built-in docs/static paths, then...
+        docs_url=None,
+        swagger_ui_oauth2_redirect_url=None,
     )
+    # ...replace them w/ locally hosted paths
+    patch_fastapi(app)
 
     @app.exception_handler(TodoError)
     async def todo_sends_501(req: Request, exc: TodoError) -> JSONResponse:
