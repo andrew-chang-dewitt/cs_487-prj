da399ac9c2f4f1458cf182314bd106f0a7b80db9
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2026-04-23 10:24:43 -0500

    feat(backend): begin implementing db-backed storage

--- backend/src/context.py
+++ backend/src/context.py
@@ -1,6 +1,7 @@
 """Shared state used accross the application."""
 
 from .config import Config
+from .database import Client, create_client, create_conn_config
 
 
 class Context:
@@ -11,10 +12,17 @@ class Context:
     - database client
     """
 
-    # FIXME: have this hold a database connection when that's implemented!
-    database: bool
+    database: Client
     root_path: str
 
     def __init__(self, config: Config = Config()) -> None:
-        self.database = True
+        self.database = create_client(
+            create_conn_config(
+                user=config.db_user,
+                password=config.db_password,
+                host=config.db_host,
+                port=config.db_port,
+                database=config.db_name,
+            )
+        )
         self.root_path = config.root_path
